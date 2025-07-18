a7a1816a938bd05298dfd95292a715bc190613a9
memory-h <gusdud9124@gmail.com>
2025-07-18 12:52:06 +0900

    feat: ci: Github Actions 기반 ECS 배포 파이프라인 추가

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 from app.routes import predict
 from app.routes import admin
+from app.routes import health_check
 
 app = FastAPI()
 app.include_router(admin.router)
 app.include_router(predict.router)
+
+app.include_router(health_check.router)
